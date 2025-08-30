9d0367d58c5049296613d1a8c73af3a34e1a7559
svetakanani <svetakanani06@gmail.com>
2025-08-30 09:26:50 +0530

    String 2 upload

---
+++ String2.java
@@ -0,0 +1,25 @@
+package demo;
+
+public class String2 {
+	
+	public int vowelCount(String str) {
+		int count=0;
+		for(int i=0;i<str.length();i++) {
+			str=str.toLowerCase();
+			char ch = str.charAt(i);
+			if(ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u') {
+				count++;
+			}
+		}
+		return count;
+	}
+
+	public static void main(String[] args) {
+		String str = "GrowItSkill";
+		String2 s = new String2();
+		//s.vowelCount(str);
+		System.out.println(s.vowelCount(str));
+
+	}
+
+}
